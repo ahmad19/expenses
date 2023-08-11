a43560f9be5d974fa2ec797da23123ca8fd23e8e
Ahmad hamza <ahmadhamza19@gmail.com>
2023-08-11 22:34:34 +0530

    table by category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+  def index
+    @transactions_grouped_with_total_amount = Rails.cache.fetch("transactions_grouped_with_total_amount")
+    @transactions_grouped = Rails.cache.fetch("transactions_grouped")
+  end
 end
--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -4,11 +4,11 @@ class UploadController < ApplicationController
   def create
     file_data = File.read(params[:file].tempfile)
     transactions_array = file_data.split(/\r\n/)[2..-1]
-    result = transactions_array.inject([]) do |final, transaction|
+    @result = transactions_array.inject([]) do |final, transaction|
       transaction_data = transaction.split(",")
       if transaction_data[4].strip.to_f == 0.0
         final << {
-          date: Date.strptime(transaction_data[0].strip, "%m/%d/%y"),
+          date: Date.strptime(transaction_data[0].strip, "%d/%m/%y").to_s,
           type: transaction_data[1].strip,
           amount: transaction_data[3].strip.to_f,
           category: Categorizer.new(transaction_data[1].strip).call
@@ -16,6 +16,12 @@ class UploadController < ApplicationController
       end
       final
     end
-    redirect_to root_path, notice: "File uploaded successfully and procssed!"
+    @general_result = @result.group_by { |t| t[:category] }.map { |k, v| [k, v.sum { |t| t[:amount] }] }.to_h
+    @result = @result.group_by { |t| t[:category] }
+    # save result into rails cache for 1 hour
+    Rails.cache.clear
+    Rails.cache.write("transactions_grouped_with_total_amount", @general_result, expires_in: 1.hour)
+    Rails.cache.write("transactions_grouped", @result, expires_in: 1.hour)
+    redirect_to root_path
   end
 end
